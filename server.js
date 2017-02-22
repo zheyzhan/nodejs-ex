f4fb5ac7098fea08828345ec1281eea94db6a04d
lubangnshi <lubangnshi@linux-zrhr.suse>
2017-02-22 18:40:09 +0800

    add repo

--- server.js
+++ server.js
@@ -1,107 +1,228 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    eps     = require('ejs'),
-    morgan  = require('morgan');
+var express = require('express');
+var app = express();
+var MongoClient=require('mongodb').MongoClient;
+var STR='mongodb://localhost:27017/';
+//get# http://localhost:8081/users/zf
+//post# http://localhost:8081/users?name=lx&password=536321&username=lx&created=201702180000
+//delete# http://localhost:8081/users/zf
+//put# http://localhost:8081/users/id=7?username=lx&password=123456
+app.use(express.static('public'));
+app.get('/users', function (req, res) {
+   MongoClient.connect(STR,function(err,db)
+  {
+   console.log("connect successfully!");
+    var myDB=db.db("web");
+    var da;
+    myDB.collection("users",function(err,collection)
+    {
+         getAll(collection,function(da) {
+          console.log(da); 
+           res.json(da);
+           res.status(200);
+           res.end();
+           myDB.close();
+        });
+        
+    });
     
-Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
-
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
-
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
-var db = null,
-    dbDetails = new Object();
-
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
   });
-};
+})
+
+app.get('/users/[a-zA-z]+', function (req, res) {
+     var username=(req.path).substring(7);
+     MongoClient.connect(STR,function(err,db)
+  {
+   //console.log("connect successfully!");
+    var myDB=db.db("web");
+    var da;
+    myDB.collection("users",function(err,collection)
+    {
+          getOne(collection,username,function(da) {  
+           if(da.length==0)
+           {
+            res.status(404);
+           }
+           else
+           {
+            res.json(da);
+            res.status(200);
+           }
+           res.end();
+           myDB.close();
+        });
+        
+    });
+    
+  });
+})
 
 app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+      res.end("Welcome Get!");
+})
+
+app.post('/', function (req, res) {
+      res.end("Welcome Post!");
+
+})
+
+app.post('/users?', function (req, res) {
+      var name=req.query.name;
+      var password=req.query.password;
+      var username=req.query.username;
+      var created=req.query.created;
+      console.log(username+" "+password+" "+name+" "+created);
+      MongoClient.connect(STR,function(err,db)
+   {
+    console.log("connect successfully!");
+    var myDB=db.db("web");
+     var IDnumber;
+     getNextID(myDB,"UserID",function(result)
+     {
+         IDnumber=result;
+         console.log("ID:"+IDnumber);
+    var doc={"_id":IDnumber,"Username":username,"Password":password,"Name":name,"Created":created};
+    myDB.collection("users",function(err,collection)
+        {
+          collection.insert(doc,function(err,results)
+          {
+           if(err)
+           {
+           var status={ok:0};
+           res.status(409);
+           }
+           else
+           {
+           var status={ok:1}; 
+           res.status(201);
+           } 
+           res.json(status);
+            res.end();
+            myDB.close();
+          });
+        });
+     });        
     });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
+})
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
+app.put('/users/id=[0-9]+?', function (req, res) {
+      var path=req.path.split("/");
+      var id=path[2].substr(3);
+      var query=JSON.stringify(req.query);
+      query=query.replace(/name/,"Name").replace(/username/,"Username").replace(/password/,"Password");
+      
+      console.log(query);
+      MongoClient.connect(STR,function(err,db)
+   {
+    console.log("connect successfully!");
+    var myDB=db.db("web");
+    myDB.collection("users",function(err,collection)
+        {
+          var queryid={"_id":Number(id)};
+           console.log(queryid);
+          var update={"$set":JSON.parse(query)};
+          var options={"upsert":false,"multi":true};
+          collection.update(queryid,update,options,function(err,results){
+                result=JSON.parse(results);
+                if(result.n==0)
+                {
+                	res.status(404);
+
+                }
+                else
+                {
+                	res.send("Put succeeds");
+                	res.status(200);
+                }
+                res.end();
+                myDB.close();
+          });
+
+        });       
     });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
+     
+})
 
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
+app.delete('/users/[a-zA-z]+', function (req, res) {
+     var username=(req.path).substring(7);
+     MongoClient.connect(STR,function(err,db)
+  {
+   //console.log("connect successfully!");
+    var myDB=db.db("web");
+    var da;
+    myDB.collection("users",function(err,collection)
+    {
+         var query={"Username":username};
+         // collection.remove(query,function(err,result){
+         //   res.send("Deletion succeeds");
+         //   res.status(200);
+         //   res.end();
+         //   myDB.close();
+         // });
+         collection.findAndRemove(
+          query,
+          [['_id','asc']],
+          {remove:true},
+          function(err,doc){
+             //console.log("Message:"+doc.lastErrorObject.n);
+             if(!doc.lastErrorObject.n)
+             {
+                res.status(404);
+             }
+             
+             else
+             {  res.send("Deletion succeeds");
+                res.status(200);
+             }    
+             res.end();
+             myDB.close();
+          });
+          
+    });
+        
+  });
+})
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
+function getAll(collection,callback)
+{
+  var sorter=[["_id",1]];
+  var cursor=collection.find({},{Password:0});
+  cursor=cursor.sort(sorter);
+  cursor.toArray().then(function(da){
+    callback(da);
+  })
+}
 
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+function getOne(collection,user,callback)
+{
+  var query={"Username":user};
+  console.log(query);
+  var cursor=collection.find(query,{Password:0});
+  cursor.toArray().then(function(da){
+    callback(da);
+  })
+}
 
-module.exports = app ;
+function getNextID(myDB,SequenceName,callback)
+{
+  myDB.collection("counters",function(err,collection)
+    {
+        collection.findAndModify(
+          {_id:SequenceName},
+          [['_id','asc']],
+          {$inc:{sequence_value:1}},
+          {new:true},
+          function(err,doc) {
+            callback(doc.value.sequence_value);
+          });      
+    });
+   
+}
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("访问地址为 http://%s:%s", host, port)
+
+})
